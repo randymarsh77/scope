3261f9bc51030e33967fc0ab504ae5ab692caa63
randymarsh77 <mattpsbs@gmail.com>
2024-12-31 11:19:27 -0800

    Make Scope Sendable with async callback and async dispose.

--- Sources/Scope/Scope.swift
+++ Sources/Scope/Scope.swift
@@ -1,22 +1,29 @@
 import IDisposable
 
-public typealias DisposeFunc = () -> Void
+public typealias DisposeFunc = @Sendable () async -> Void
 
-public class Scope: IDisposable {
-	var disposeFunc: DisposeFunc?
+@available(iOS 13.0.0, *)
+@available(macOS 10.15.0, *)
+public final actor Scope: Sendable, IAsyncDisposable {
+	public var isDisposed = false
+	private let disposeFunc: DisposeFunc?
 
 	public init(dispose: DisposeFunc?) {
 		disposeFunc = dispose
 	}
 
-	public func dispose() {
-		disposeFunc?()
-		disposeFunc = nil
+	public func dispose() async {
+		if (isDisposed) {
+			return;
+		}
+
+		isDisposed = true
+		await disposeFunc?()
 	}
 
 	public func transfer() -> Scope {
 		let newScope = Scope(dispose: disposeFunc)
-		disposeFunc = nil
+		isDisposed = true
 		return newScope
 	}
 }
--- Tests/ScopeTests/ScopeTests.swift
+++ Tests/ScopeTests/ScopeTests.swift
@@ -2,37 +2,55 @@ import XCTest
 
 @testable import Scope
 
+actor Counter: Sendable
+{
+	var value = 0
+
+	public func increment() {
+		value += 1
+	}
+
+	public func getValue() -> Int {
+		return value
+	}
+}
+
 class ScopeTests: XCTestCase {
-	// Dispose should nil the callback
-	func testDisposeCleanup() {
+	// Dispose should set isDisposed
+	func testDisposeCleanup() async {
 		let scope = Scope {}
-		scope.dispose()
-		XCTAssertNil(scope.disposeFunc)
+		await scope.dispose()
+		let isDisposed = await scope.isDisposed
+		XCTAssertTrue(isDisposed)
 	}
 
 	// Dispose should call the callback
-	func testDispose() {
-		var x = 0
+	func testDispose() async {
+		let counter = Counter()
 		let scope = Scope {
-			x += 1
+			await counter.increment()
 		}
-		scope.dispose()
-		XCTAssertEqual(x, 1)
-		scope.dispose()
-		XCTAssertEqual(x, 1, "Multiple Dispose should be safe, but a noop")
+		await scope.dispose()
+		let firstValue = await counter.getValue()
+		XCTAssertEqual(firstValue, 1)
+		await scope.dispose()
+		let secondValue = await counter.getValue()
+		XCTAssertEqual(secondValue, 1, "Multiple Dispose should be safe, but a noop")
 	}
 
 	// Transfer should create a new scope with the same callback,
 	// leaving the original scope without a callback
-	func testTransfer() {
-		var x = 0
+	func testTransfer() async {
+		let counter = Counter()
 		let scope = Scope {
-			x += 1
+			await counter.increment()
 		}
-		let transferred = scope.transfer()
-		scope.dispose()
-		XCTAssertEqual(x, 0, "Scope should have been transferred")
-		transferred.dispose()
-		XCTAssertEqual(x, 1, "Transferred scope should call the original callback")
+		let transferred = await scope.transfer()
+		await scope.dispose()
+		let firstValue = await counter.getValue()
+		XCTAssertEqual(firstValue, 0, "Scope should have been transferred")
+		await transferred.dispose()
+		let secondValue = await counter.getValue()
+		XCTAssertEqual(secondValue, 1, "Transferred scope should call the original callback")
 	}
 }
